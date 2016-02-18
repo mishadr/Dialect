b1cb896b132d70438eefc2e3f802032589cc146e
misha <aytm@mail.ru>
2016-02-18 16:28:53 +0300

    Seq2seq model runs.

--- src/tf_seq2seq.py
+++ src/tf_seq2seq.py
@@ -1,8 +1,10 @@
 import numpy as np
-import os
-import sys
 import tensorflow as tf
 import time
+
+from matplotlib import cm
+from matplotlib.pyplot import figure, clf, plot, ginput, show, imshow, subplot
+
 from sklearn import cross_validation
 from tensorflow.models.rnn import rnn_cell
 from tensorflow.models.rnn import seq2seq
@@ -13,35 +15,45 @@ from tensorflow.python.ops import rnn
 from tensorflow.python.framework import dtypes
 
 
+tf.app.flags.DEFINE_integer("batch_size", 1, "Batch size to use during training.")
+
+
 def inference(feature_size, decoder_vocab_size, feature_placeholders, output_placeholders,
-              info_placeholder, feed_previous, reuse_codecs=None):
+              feed_previous, reuse_codecs=None):
     """Build the seq2seq model up to where it may be used for inference.
     Args:
     Returns:
       computed logits (predictions).
     """
 
-    # Firstly we should map speech features (which is 128-vectors of floats) to RNN-friendly inputs
+    # NOTE: Firstly we could map speech features (which is 128-vectors of floats) to RNN-friendly inputs
     # (should be ints). It is possible to use maxout network (like in http://arxiv.org/pdf/1412.1602.pdf,
     # where it was preliminary trained with HMMs and tuned).
 
     # Assuming batch_size = 1
     # Emulating get_batch() and step() from seq2seq_model.py
 
-    # I'd like to prune inputs and outputs according to their actual lengths, but I don't know the way to do it
-    input_size = len(feature_placeholders)
-    output_size = len(output_placeholders)
+    # TODO I'd like to prune inputs and outputs according to their actual lengths, but I don't know the way to do it
+    # input_size = len(feature_placeholders)
+    # output_size = len(output_placeholders)
+    # encoder_inputs = tf.unpack(tf.slice(tf.pack(feature_placeholders),
+    #              tf.constant([0, 0, 0], dtype=tf.int32),
+    #              tf.pack([input_size, tf.shape(feature_placeholders[0])[0], tf.shape(feature_placeholders[0])[1]])), input_size)
+    #
+    # decoder_inputs = tf.unpack(tf.slice(tf.pack(output_placeholders),
+    #              tf.constant([0, 0, 0], dtype=tf.int32),
+    #              tf.pack([output_size, tf.shape(feature_placeholders)[0], tf.shape(feature_placeholders)[1]])), output_size)
+
 
     # encoder inputs are just reversed list of features
-    encoder_inputs = list(reversed(feature_placeholders[0:input_size]))
+    # TODO is the reversed order really better?
+    encoder_inputs = list(reversed(feature_placeholders))
 
     # decoder inputs are list of labels with symbol 'GO' prepended, transformed to one-hot form.
-    # FIXME GO_ID = 1, what if label class = 1 too?
-    # Assuming one-hot form
-    decoder_inputs = output_placeholders[0:output_size]
+    decoder_inputs = output_placeholders
 
     # run basic seq2seq encoder-decoder
-    size = 300
+    size = 200
     cell_enc_in = rnn_cell.LSTMCell(num_units=size, input_size=feature_size)
     cell_internal = rnn_cell.LSTMCell(num_units=size, input_size=size)
     cell_dec_in = rnn_cell.LSTMCell(num_units=size, input_size=decoder_vocab_size)
@@ -65,11 +77,18 @@ def calculate_loss(output_size, logits, labels_placeholders, decoder_vocab_size)
     # Now I should calculate loss analogously to sequence_loss() from seq2seq_model.py
 
     # Our targets are labels sequence (equals output sequence shifted by one).
-    targets = labels_placeholders[0:output_size]
+    targets = labels_placeholders
 
-    # target weights are 1s except the last
-    target_weights = [tf.ones(shape=[1], dtype=tf.float32) for _ in xrange(len(targets))]
-    target_weights[-1] = tf.zeros(shape=[1], dtype=tf.float32)
+    # target weights are 1s for valuable input symbols and zeros for PADs and the last (for <EOS>)
+    ones = tf.slice(tf.ones(shape=[len(targets), 1], dtype=tf.float32),
+                     tf.constant([0, 0], shape=[2], dtype=tf.int32),
+                     tf.pack([output_size, tf.constant(1)]))
+
+    zeros = tf.slice(tf.zeros(shape=[len(targets), 1], dtype=tf.float32),
+                    tf.pack([output_size, tf.constant(0)]),
+                    tf.constant([-1, 1], shape=[2], dtype=tf.int32))
+    tw = tf.concat(0, [ones, zeros])
+    target_weights = tf.unpack(tw, num=len(targets))
 
     loss = seq2seq.sequence_loss(logits, targets, target_weights, decoder_vocab_size, )
     return loss
@@ -113,12 +132,12 @@ def to_one_hot(classes, output_size):
 
 
 def evaluation(feature_size, decoder_vocab_size, inputs_placeholders, output_placeholders,
-               info_placeholder, labels_placeholders):
+               labels_placeholders):
     """Evaluate the quality of the logits at predicting the label.
     """
 
     logits = inference(feature_size, decoder_vocab_size, inputs_placeholders, output_placeholders,
-                       info_placeholder, False, True)
+                       False, True)
     # # For a classifier model, we can use the in_top_k Op.
     # # It returns a bool tensor with shape [batch_size] that is true for
     # # the examples where the label's is was in the top k (here k=1)
@@ -127,7 +146,10 @@ def evaluation(feature_size, decoder_vocab_size, inputs_placeholders, output_pla
     # # Return the number of true entries.
     # return tf.reduce_sum(tf.cast(correct, tf.int32))
 
-    return [tf.argmax(logit, tf.constant(1, tf.int32)) for logit in logits]
+    # Apply softmax just for evaluation (sequence_loss applies its own softmax
+    logits = [tf.nn.softmax(logit) for logit in logits]
+
+    return logits#, [tf.argmax(logit, tf.constant(1, tf.int32)) for logit in logits]
     # return tf.zeros(shape=[1], dtype=dtypes.int32)
 
 
@@ -136,9 +158,9 @@ def train_and_test(alphabet, feat_labs):
 
     # Extending alphabet with special symbols
     end = len(alphabet)
-    alphabet[data_utils._UNK] = 0
-    alphabet[data_utils._GO] = end + data_utils.GO_ID
+    # alphabet[data_utils._UNK] = 0
     alphabet[data_utils._PAD] = end + data_utils.PAD_ID
+    alphabet[data_utils._GO] = end + data_utils.GO_ID
     alphabet[data_utils._EOS] = end + data_utils.EOS_ID
 
     feature_size = np.shape(feat_labs[0][0])[1]
@@ -151,18 +173,22 @@ def train_and_test(alphabet, feat_labs):
 
     max_inputs = np.max([len(xs) for xs, _ in feat_labs])
     max_outputs = np.max([len(ys) for _, ys in feat_labs])
-    learning_rate = 0.05
+    # TODO modify code to using any batch size
+    batch_size = 1
 
     # Tell TensorFlow that the model will be built into the default Graph.
     with tf.Graph().as_default():
         # Generate placeholders for the data and labels.
-        inputs_placeholders = [tf.placeholder(tf.float32, shape=(1, feature_size), name="input_{0}".format(i))
+        inputs_placeholders = [tf.placeholder(tf.float32, shape=(batch_size, feature_size), name="input_{0}".format(i))
                                for i in xrange(max_inputs)]
-        output_placeholders = [tf.placeholder(tf.float32, shape=(1, decoder_vocab_size), name="output_{0}".format(i))
+        output_placeholders = [tf.placeholder(tf.float32, shape=(batch_size, decoder_vocab_size), name="output_{0}".format(i))
                                for i in xrange(max_outputs + 1)]
-        labels_placeholders = [tf.placeholder(tf.int32, shape=(1), name="label_{0}".format(i))
+        labels_placeholders = [tf.placeholder(tf.int32, shape=(batch_size), name="label_{0}".format(i))
                                for i in xrange(max_outputs + 1)]
-        info_placeholder = tf.placeholder(tf.int32, shape=(2), name="info")
+        input_size = tf.placeholder(tf.int32, shape=[])
+        output_size = tf.placeholder(tf.int32, shape=[])
+
+        learning_rate = tf.Variable(0.05, trainable=False)
 
         def prepare_data(xs, ys):
             # Input feed: encoder inputs, decoder inputs, labels and actual lengths of inputs and output.
@@ -172,15 +198,20 @@ def train_and_test(alphabet, feat_labs):
             labels = [np.reshape(l, (1,)) for l in np.append(ys, alphabet[data_utils._EOS])]
             # print "inputs: %s, outputs: %s, labels: %s" %\
             #       (str(np.shape(inputs)), str(np.shape(outputs)),str(np.shape(labels)))
-            input_dict = {pl: data for pl, data in
 
+            input_PAD = np.zeros(shape=(1, feature_size))
+            label_PAD = np.array([0], dtype=np.int32)
+            output_PAD = to_one_hot(label_PAD, decoder_vocab_size)
+
+            input_dict = {pl: data for pl, data in
                           zip(inputs_placeholders,
-                              inputs + (len(inputs_placeholders) - len(inputs)) * [np.zeros(shape=(1, feature_size))])
+                              inputs + (len(inputs_placeholders) - len(inputs)) * [input_PAD])
                           + zip(output_placeholders,
-                                outputs + (len(output_placeholders) - len(outputs)) * [np.zeros(shape=(1, decoder_vocab_size))])
+                                outputs + (len(output_placeholders) - len(outputs)) * [output_PAD])
                           + zip(labels_placeholders,
-                                labels + (len(labels_placeholders) - len(labels)) * [np.zeros(shape=(1), dtype=np.int32)])}
-            input_dict[info_placeholder] = (len(inputs), len(outputs))
+                                labels + (len(labels_placeholders) - len(labels)) * [label_PAD])}
+            input_dict[input_size] = len(xs)
+            input_dict[output_size] = len(ys)
             return input_dict
 
         def do_eval(sess, eval_correct, test_set):
@@ -192,23 +223,36 @@ def train_and_test(alphabet, feat_labs):
             for xs, ys in test_set:
                 input_feed = prepare_data(xs, ys)
                 # true_count += sess.run(eval_correct, feed_dict=input_feed)
-                res = sess.run(eval_correct, feed_dict=input_feed)
+                logits = sess.run(eval_correct, feed_dict=input_feed)
+
+                res = [np.argmax(logit, 1) for logit in logits]
                 res = list(np.resize(res, (len(res))))
-                if alphabet[data_utils._EOS] in res:
-                    res = res[:res.index(alphabet[data_utils._EOS])]
+                # # Cutting result after <EOS> symbol, if present
+                # if alphabet[data_utils._EOS] in res:
+                #     res = res[:res.index(alphabet[data_utils._EOS])]
+
                 print res, ys
+
+                # logits = np.reshape(np.array(logits), (len(logits), decoder_vocab_size))
+                # figure("prediction")
+                # clf()
+                # subplot(121);
+                # imshow(logits.T, cmap=cm.hot, interpolation='nearest')
+                # subplot(122);
+                # imshow((to_one_hot(ys, decoder_vocab_size)).T, cmap=cm.hot, interpolation='nearest')
+                # # subplot(313); imshow(freq.T, cmap=cm.hot, interpolation='nearest')
+                # ginput(1, 11110.01)
 
             precision = 1.0 * true_count / num_examples
             print('  Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
                   (num_examples, true_count, precision))
 
-
         # Build a Graph that computes predictions from the inference model.
-        logits = inference(feature_size, decoder_vocab_size, inputs_placeholders, output_placeholders,
-                           info_placeholder, True)
+        logits = inference(feature_size, decoder_vocab_size, inputs_placeholders, output_placeholders, True)
 
         # Add to the Graph the Ops for loss calculation.
-        loss = calculate_loss(max_outputs+1, logits, labels_placeholders, decoder_vocab_size)
+        # TODO n_outputs must coincide with actual input length
+        loss = calculate_loss(output_size, logits, labels_placeholders, decoder_vocab_size)
         print('1')
 
         # Add to the Graph the Ops that calculate and apply gradients.
@@ -217,7 +261,7 @@ def train_and_test(alphabet, feat_labs):
 
         # # Add the Op to compare the logits to the labels during evaluation.
         eval_correct = evaluation(feature_size, decoder_vocab_size, inputs_placeholders, output_placeholders,
-                                  info_placeholder, labels_placeholders)
+                                  labels_placeholders)
 
         # Build the summary operation based on the TF collection of Summaries.
         summary_op = tf.merge_all_summaries()
@@ -265,8 +309,8 @@ def train_and_test(alphabet, feat_labs):
                 summary_writer.add_summary(summary_str, step)
 
                 # Save a checkpoint and evaluate the model periodically.
-                if (step+1) % 100 == 0 or (step + 1) == n_steps:
-                    saver.save(sess, "report", global_step=step)
+                if (step + 1) % 400 == 0 or (step + 1) == n_steps:
+                    saver.save(sess, "save/state", global_step=step)
 
                     # Evaluate against the training set.
                     print('Training Data Eval:')
@@ -275,3 +319,6 @@ def train_and_test(alphabet, feat_labs):
                     # Evaluate against the test set.
                     print('Test Data Eval:')
                     do_eval(sess, eval_correct, test_set)
+
+                    # Decrease learning rate
+                    learning_rate.assign(learning_rate * 0.9)
